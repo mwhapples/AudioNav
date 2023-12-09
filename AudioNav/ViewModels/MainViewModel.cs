5e8629f78c63d8ba8903527d76e961f6fc8d26b8
Michael Whapples <software@mdw.onl>
2023-12-09 13:03:45 +0000

    Add UI for changing course.

--- AudioNav/ViewModels/MainViewModel.cs
+++ AudioNav/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using AudioNav.Utils;
 using ReactiveUI;
 using System;
 using System.Numerics;
+using System.Reactive;
 using System.Reactive.Linq;
 
 namespace AudioNav.ViewModels;
@@ -13,6 +14,7 @@ public class MainViewModel : ReactiveObject, IActivatableViewModel
     public MainViewModel()
     {
         audio_Compass = new();
+        course = audio_Compass.Course.Select(x => (int)x.Degrees).ToProperty(this, x => x.Course);
         compassFilterRate = audio_Compass.FilterRate.ToProperty(this, x => x.CompassFilterRate);
         filteredCompass = audio_Compass.CompassHeading.ToProperty(this, x => x.FilteredCompass);
         headingText = this.WhenAnyValue(x => x.FilteredCompass).Select(x => x switch
@@ -20,14 +22,15 @@ public class MainViewModel : ReactiveObject, IActivatableViewModel
             CompassData.HeadingReading r => r.Heading.Degrees.ToString("000"),
             _ => "---"
         }).ToProperty(this, x => x.HeadingText);
+        IncrementCourseCommand = ReactiveCommand.Create<int>(x => Course += x);
     }
 
     public ViewModelActivator Activator { get; } = new();
-    private Heading course = Heading.FromDegrees(0);
-    public Heading Course
+    private readonly ObservableAsPropertyHelper<int> course;
+    public int Course
     {
-        get => course;
-        set => this.RaiseAndSetIfChanged(ref course, value);
+        get => course.Value;
+        set => audio_Compass.ChangeCourse(Heading.FromDegrees(value));
     }
     private readonly ObservableAsPropertyHelper<int> compassFilterRate;
     public int CompassFilterRate
@@ -39,4 +42,5 @@ public class MainViewModel : ReactiveObject, IActivatableViewModel
     public CompassData FilteredCompass => filteredCompass.Value;
     private readonly ObservableAsPropertyHelper<string> headingText;
     public string HeadingText => headingText.Value;
+    public ReactiveCommand<int, Unit> IncrementCourseCommand { get; }
 }
--- AudioNav/Views/MainPage.xaml.cs
+++ AudioNav/Views/MainPage.xaml.cs
@@ -18,6 +18,15 @@ public partial class MainPage : ReactiveContentPage<MainViewModel>
         {
             this.OneWayBind(ViewModel, x => x.HeadingText, x => x.HeadingLabel.Text).DisposeWith(disposables);
             this.Bind(ViewModel, vm => vm.CompassFilterRate, v => v.CompassFilterRateSlider.Value).DisposeWith(disposables);
+            this.OneWayBind(ViewModel, vm => vm.IncrementCourseCommand, v => v.LargeDecrementButton.Command);
+            this.LargeDecrementButton.CommandParameter = -10;
+            this.OneWayBind(ViewModel, vm => vm.IncrementCourseCommand, v => v.DecrementButton.Command);
+            this.DecrementButton.CommandParameter = -1;
+            this.OneWayBind(ViewModel, vm => vm.Course, v => v.CourseLabel.Text);
+            this.OneWayBind(ViewModel, vm => vm.IncrementCourseCommand, v => v.IncrementButton.Command);
+            this.IncrementButton.CommandParameter = 1;
+            this.OneWayBind(ViewModel, vm => vm.IncrementCourseCommand, v => v.LargeIncrementButton.Command);
+            this.LargeIncrementButton.CommandParameter = 10;
         });
     }
 }
