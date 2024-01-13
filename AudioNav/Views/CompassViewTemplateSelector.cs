c611908688c9a74c90485fd94ef6dfd86cf055b8
Michael Whapples <software@mdw.onl>
2024-01-13 19:50:20 +0000

    Change to using DataTemplateSelector for the dynamic view.

---
+++ AudioNav/Views/CompassViewTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using AudioNav.ViewModels;
+using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace AudioNav.Views;
+
+internal class CompassViewTemplateSelector : DataTemplateSelector
+{
+    private readonly IImmutableDictionary<Type, DataTemplate?> _templateDictionary = new Dictionary<Type, DataTemplate?>
+    {
+        [typeof(CourseToHeadingViewModel)] = new DataTemplate(typeof(CourseToHeadingView)),
+        [typeof(FilterRateViewModel)] = new DataTemplate(typeof(FilterRateView)),
+        [typeof(OutputSelectorViewModel)] = new DataTemplate(typeof(OutputSelectorView)),
+        [typeof(SensorSelectorViewModel)] = new DataTemplate(typeof(SensorSelectorView)),
+        [typeof(SimpleCourseViewModel)] = new DataTemplate(typeof(SimpleCourseView)),
+        [typeof(SimpleHeadingViewModel)] = new DataTemplate(typeof(SimpleHeadingView))
+    }.ToImmutableDictionary();
+    public DataTemplate? UnknownTemplate { get; set; }
+    protected override DataTemplate? OnSelectTemplate(object item, BindableObject container)
+    {
+        return _templateDictionary.GetValueOrDefault(item.GetType(), UnknownTemplate);
+    }
+}
